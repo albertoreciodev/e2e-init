273203ba9d3939ea4e4472112ef56d64e4255eb7
Joan León <joan.leon@gmail.com>
2022-11-26 10:26:23 +0100

    Remove idProject and update the specPattern

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "et3bg9",
   reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
     },
-    specPattern: "cypress/e2e/**/*/.{cy,spec}.{js,jsx,ts,tsx}",
+    specPattern: "cypress/e2e/1-getting-started/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "https://example.cypress.io",
     pageLoadTimeout: 1000,
     viewportHeight: 950,
